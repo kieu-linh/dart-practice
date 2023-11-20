e91f80fd08affa1c34d7295c6c1803bd1f4c9879
Kieu Linh <hothikieulinh1407@gmail.com>
2023-11-20 15:35:11 +0700

    create project dart

---
+++ dart_practice_project/test/dart_practice_project_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_practice_project/dart_practice_project.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
